78b4a67fa39e4b3281b8e8363097cb4497a4c205
Raghav Korde <raghavkorde4@gmail.com>
2025-04-17 13:33:24 +0530

    fixed host

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -1,3 +1,13 @@
+export const IP_ADDRESS = "192.168.0.217";
+let HOST: string;
+
+if (!IP_ADDRESS) {
+    console.log("IP_ADDRESS is not set. Using localhost");
+    HOST = "localhost";
+} else {
+    HOST = IP_ADDRESS;
+}
+
 export const config = {
     MANAGER_BACKEND_PORT: 3001,
     MANAGER_FRONTEND_PORT: 5001,
@@ -5,11 +15,10 @@ export const config = {
     DEV_TOOLS_FRONTEND_PORT: 5002,
     TANGO_BACKEND_MANAGER_PORT: 3003,
     FRIDA_SERVER: 3004,
-    MANAGER_BACKEND_URL: "http://localhost:3001",
-    MANAGER_FRONTEND_URL: "http://localhost:5001",
-    DEV_TOOLS_BACKEND_URL: "http://localhost:3002",
-    DEV_TOOLS_FRONTEND_URL: "http://localhost:5002",
-    TANGO_BACKEND_MANAGER_URL: "http://localhost:3003",
-    FRIDA_SERVER_URL: "http://localhost:3004",
+    MANAGER_BACKEND_URL: `http://${HOST}:3001`,
+    MANAGER_FRONTEND_URL: `http://${HOST}:5001`,
+    DEV_TOOLS_BACKEND_URL: `http://${HOST}:3002`,
+    DEV_TOOLS_FRONTEND_URL: `http://${HOST}:5002`,
+    TANGO_BACKEND_MANAGER_URL: `http://${HOST}:3003`,
+    FRIDA_SERVER_URL: `http://${HOST}:3004`,
 };
-
